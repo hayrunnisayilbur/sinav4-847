b564d0441733abc20ce790bfcff4991350a6c967
hayrunnisayilbur <145451185+hayrunnisayilbur@users.noreply.github.com>
2023-11-21 13:43:38 +0300

    Add files via upload

---
+++ sinav4-847/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace sinav4_847
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOlustur = new System.Windows.Forms.Button();
+            this.lbSayilar1 = new System.Windows.Forms.ListBox();
+            this.btnAktar = new System.Windows.Forms.Button();
+            this.lbSayilar2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnOlustur
+            // 
+            this.btnOlustur.Location = new System.Drawing.Point(34, 42);
+            this.btnOlustur.Name = "btnOlustur";
+            this.btnOlustur.Size = new System.Drawing.Size(128, 25);
+            this.btnOlustur.TabIndex = 0;
+            this.btnOlustur.Text = "20 adet sayı oluştur";
+            this.btnOlustur.UseVisualStyleBackColor = true;
+            this.btnOlustur.Click += new System.EventHandler(this.btnOlustur_Click);
+            // 
+            // lbSayilar1
+            // 
+            this.lbSayilar1.FormattingEnabled = true;
+            this.lbSayilar1.Location = new System.Drawing.Point(34, 89);
+            this.lbSayilar1.Name = "lbSayilar1";
+            this.lbSayilar1.Size = new System.Drawing.Size(128, 238);
+            this.lbSayilar1.TabIndex = 1;
+            // 
+            // btnAktar
+            // 
+            this.btnAktar.Location = new System.Drawing.Point(190, 181);
+            this.btnAktar.Name = "btnAktar";
+            this.btnAktar.Size = new System.Drawing.Size(123, 24);
+            this.btnAktar.TabIndex = 2;
+            this.btnAktar.Text = "50 - 70 arasını aktar";
+            this.btnAktar.UseVisualStyleBackColor = true;
+            this.btnAktar.Click += new System.EventHandler(this.btnAktar_Click);
+            // 
+            // lbSayilar2
+            // 
+            this.lbSayilar2.FormattingEnabled = true;
+            this.lbSayilar2.Location = new System.Drawing.Point(347, 89);
+            this.lbSayilar2.Name = "lbSayilar2";
+            this.lbSayilar2.Size = new System.Drawing.Size(128, 238);
+            this.lbSayilar2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbSayilar2);
+            this.Controls.Add(this.btnAktar);
+            this.Controls.Add(this.lbSayilar1);
+            this.Controls.Add(this.btnOlustur);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOlustur;
+        private System.Windows.Forms.ListBox lbSayilar1;
+        private System.Windows.Forms.Button btnAktar;
+        private System.Windows.Forms.ListBox lbSayilar2;
+    }
+}
+
---
+++ sinav4-847/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sinav4_847
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOlustur_Click(object sender, EventArgs e)
+        {
+            lbSayilar1.Items.Clear();
+            lbSayilar2.Items.Clear();
+
+            Random random = new Random();
+            for (int i = 0; i < 20; i++)
+            { // 20 defa çalışır
+
+                lbSayilar1.Items.Add(random.Next(1, 101));
+            }
+        }
+
+        private void btnAktar_Click(object sender, EventArgs e)
+        {
+             for ( int i = 0;1 < lbSayilar1.Items.Count; i++)
+             {
+                // i. sıradaki değeri alır
+                int sayi = Convert.ToInt32(lbSayilar1.Items[i].ToString());
+                // MessageBox.Show(sayi.ToString());
+
+                if ( sayi >=50 && sayi <= 70)
+                {
+                    lbSayilar2.Items.Add( sayi );
+                }
+             }
+        }
+    }
+}
+
